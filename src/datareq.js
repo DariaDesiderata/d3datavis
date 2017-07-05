e36c8b59d68637c57f344e1ceaa8b4b9870d006e
DariaDesiderata <daria.calwell@gmail.com>
2017-07-05 16:34:10 -0600

    refactor with scale

--- src/datareq.js
+++ src/datareq.js
@@ -8,15 +8,28 @@ let chart = d3.select(".chartArea")
     .attr('height', height)
     .attr('width', width)
 
+// set yScale to be anywhere within the predefined height less padding
+let yScale = d3.scaleLinear()
+                    .range([height-padding, 0+padding])
+
 d3.csv('../angularGeoMap.csv', (error, data) => {
-   dots = chart.selectAll('circle')
+
+// set yDomain to be max and min values of the data set
+  yDomain = d3.extent(data, (element) => {
+    return parseInt(element.angular2)
+  })
+
+// map yScale to yDomain
+  yScale.domain(yDomain)
+
+  dots = chart.selectAll('circle')
               .data(data)
               .enter()
               .append('circle')
 
-   dots.attr('r', function(d) {return d.angular2})
-       .attr('cx', function(d) {return Math.max(0 + padding, Math.random() * width - padding)})
-       .attr('cy', function(d) {return Math.max(0 + padding, Math.random() * height - padding)})
-       .attr('stroke', 'red')
-       .attr('fill', function(d) {return "hsl(" + Math.random() * 360 + ", 100%, 50%)"})
+  dots.attr('r', function(d) {return d.angular2})
+      .attr('cx', function(d) {return Math.max(0 + padding, Math.random() * width - padding)})
+      .attr('cy', (d) => {return yScale(d.angular2)}) // map data to yScale. So larger items will appear towards the larger side of the yDomain
+      .attr('stroke', 'red')
+      .attr('fill', function(d) {return "hsl(" + Math.random() * 360 + ", 100%, 50%)"})
 })
